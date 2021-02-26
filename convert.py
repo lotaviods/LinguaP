704476351b619871ab2140700bf17e6d9366c86a
lotaviods <l.otavio9099@gmail.com>
2021-02-25 21:31:55 -0300

    Alteração de view para usuario

--- convert.py
+++ convert.py
@@ -1,35 +1,38 @@
-palavra = input("Digite a frase ")
-
-def codificar(frase):
+def codificar(frase, mensagem):
     novafrase = ""
     for letra in frase:
         if letra == " ":  # verifica a existencia de espaço para não substituir a ultima letra
             novafrase += letra
         else:
             novafrase += 'p' + letra
-    return novafrase
+    return print(mensagem + novafrase)
 
-def decodificar(frase):
+
+def decodificar(frase, mensagem):
     str = ""
     lista = list(frase)
     if lista[1] != "p":
         for i in range(len(lista) - 1):
             lista[0] = ""
-            if lista[i] == "p" and lista[i+1] != "p":
+            if lista[i] == "p" and lista[i + 1] != "p":
                 lista[i] = ""
-        return str.join(lista).replace("pp", "p")
-    else: # cai nesse else apenas se a palavra digitada pelo usuario iniciar com p
+        return print(mensagem + str.join(lista).replace("pp", "p"))
+
+    else:  # cai nesse else apenas se a palavra digitada pelo usuario iniciar com p
         for i in range(len(lista) - 1):
             if i == 0:
                 lista[0] = ""
                 lista[1] = "p"
             if i >= 2:
-                if lista[i] == "p" and lista[i+1] != "p":
+                if lista[i] == "p" and lista[i + 1] != "p":
                     lista[i] = ""
-    return str.join(lista).replace("pp", "p")
+    return print(mensagem + str.join(lista).replace("pp", "p"))
 
-codifResult = (codificar(palavra))
-print(codifResult)
 
-decodifResult = (decodificar(codifResult))
-print(decodifResult)
+palavra = input("Digite a frase para codificar, caso queira somente decodificar tecle enter: ")
+
+codificar(palavra, "Palavra codificada: ")
+
+palavra = input("Digite a frase para decodificar: ")
+
+decodificar(palavra, "Frase codificada: ")
